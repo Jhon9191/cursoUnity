61b1747c16615758b01afee8d1837d2fe50cf23e
Myanameisjohndev <joaoantonio15161516@gmail.com>
2022-06-19 13:54:36 -0300

    Creating player moving

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,12 +19,14 @@ public class Player : MonoBehaviour
     private SpriteRenderer sprite;
     private Rigidbody2D rb2d;
     private Animator anim;
+    private Transform trans;
 
     void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        trans = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -32,4 +34,23 @@ public class Player : MonoBehaviour
     {
         
     }
+
+    private void FixedUpdate()
+    {
+
+        float move = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(move * speed, rb2d.velocity.y);
+
+        if (( move < 0f && facingRight ) || ( move > 0f && !facingRight)){
+            Flip();
+        }
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        trans.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+
 }
